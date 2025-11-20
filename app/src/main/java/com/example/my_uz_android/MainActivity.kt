5a75bc1ad7bd6e88898115ad4cb1007a78593538
lifeoverthinker <96396847+lifeoverthinker@users.noreply.github.com>
2025-11-20 16:03:05 +0100

    Add ClassCard and EventCard components; update Color and Type definitions

--- app/src/main/java/com/example/my_uz_android/MainActivity.kt
+++ app/src/main/java/com/example/my_uz_android/MainActivity.kt
@@ -3,24 +3,15 @@ package com.example.my_uz_android
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import com.example.my_uz_android.navigation.AppNavigation
 import com.example.my_uz_android.ui.theme.MyUZTheme
-// Importujemy moduł, aby upewnić się, że Supabase jest dostępny (opcjonalne, ale dobre dla pewności)
-import com.example.my_uz_android.di.AppModule
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        // Inicjalizacja Supabase (opcjonalnie, jeśli chcesz wymusić start od razu,
-        // choć 'by lazy' w AppModule zrobi to przy pierwszym użyciu)
-        // val supabase = AppModule.supabase
-
-        enableEdgeToEdge()
         setContent {
             MyUZTheme {
-                // Tutaj po prostu ładujemy Twoją nawigację
+                // Uruchamiamy główną nawigację aplikacji
                 AppNavigation()
             }
         }
