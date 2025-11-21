16fef658b52431f20e3763ad44b5fe03a8fb1674
lifeoverthinker <96396847+lifeoverthinker@users.noreply.github.com>
2025-11-21 13:45:51 +0100

    Add onboarding flow with LandingScreen and OnboardingViewModel; update SettingsEntity and repositories

--- app/src/main/java/com/example/my_uz_android/ui/AppViewModelProvider.kt
+++ app/src/main/java/com/example/my_uz_android/ui/AppViewModelProvider.kt
@@ -1,16 +1,27 @@
 package com.example.my_uz_android.ui
 
-import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProvider.AndroidViewModelFactory
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.my_uz_android.MyUZApplication
+import com.example.my_uz_android.ui.screens.onboarding.OnboardingViewModel
 
 object AppViewModelProvider {
     val Factory = viewModelFactory {
-        // Tu będziesz dodawać inicjalizatory ViewModeli
+        // Initializer dla OnboardingViewModel
+        initializer {
+            val application = inventoryApplication()
+            OnboardingViewModel(
+                settingsRepository = application.container.settingsRepository,
+                universityRepository = application.container.universityRepository
+            )
+        }
+
+        // Tutaj dodasz inne ViewModele w przyszłości, np. HomeViewModel
     }
 }
 
-fun CreationExtras.myUZApplication(): MyUZApplication =
-    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as MyUZApplication)
+// Funkcja pomocnicza do pobierania instancji aplikacji
+fun CreationExtras.inventoryApplication(): MyUZApplication =
+    (this[AndroidViewModelFactory.APPLICATION_KEY] as MyUZApplication)
